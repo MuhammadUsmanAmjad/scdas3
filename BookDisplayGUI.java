321d2e6c3fc405f426d377609c2792461c7c1fb6
MuhammadUsmanAmjad <l215777@lhr.nu.edu.pk>
2023-10-23 14:55:00 +0500

    Made two functions that read from a book_data.txt file and content of the book from another file and storing it in the main class as a list of book

--- BookDisplayGUI.java
+++ BookDisplayGUI.java
@@ -63,4 +63,66 @@ class Book {
         return this.contentFileName;
     }
 }
+public class BookDisplayGUI {
+    private JFrame mainFrame;
+    private JTable bookTable;
+    private JButton addButton;
+    private JButton editButton;
+    private JButton deleteButton;
+    private JFrame contentFrame;
+    private JButton popularityButton;
+    // private String Filename;
+    List<Book> books = new ArrayList<>();
+
+    public BookDisplayGUI() {
+        ReadBooksFromFile(books);
+        }
+            private void ReadBooksFromFile(List<Book> books) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("book_data.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] bookData = line.split(",");
+                if (bookData.length == 5) { // Assuming popularity count is the fourth element
+                    String title = bookData[0].trim();
+                    String author = bookData[1].trim();
+                    String publishDate = bookData[2].trim();
+                    int popularityCount = Integer.parseInt(bookData[3].trim());
+                    String contentFileName = bookData[4].trim();
+
+                    // Read content from the file
+                    String content = readContentFromFile(contentFileName);
+
+                    Book book = new Book(title, author, publishDate, content, popularityCount);
+                    book.setContentFileName(contentFileName);
+                    books.add(book);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Method to read content from a file
+    private String readContentFromFile(String fileName) {
+        StringBuilder content = new StringBuilder();
+        try (BufferedReader contentReader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = contentReader.readLine()) != null) {
+                content.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return content.toString();
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+
+            new BookDisplayGUI();
+        });
+    }
+
+        
+        }
 
